e8bbd5cee00b8e43987a387bc823b6d4786a9f74
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2010-03-31 00:35:31 +0000

    Documentado

--- src/weather/microClima.cxx
+++ src/weather/microClima.cxx
@@ -1,5 +1,6 @@
 #include "microClima.h"
 
+
 #define ALFA_SOLO 0.0425
 #define BETA_SOLO 3.9
 
@@ -26,19 +27,24 @@ MicroClima::~MicroClima(){
 
 MicroClima::MicroClima(){
 
+	/*Inicializa as variáveis com os mesmos valores das variáveis ambientais de climaTempo.
+	*É como inicializar o lagarto no meio de uma duna, sem sombras. Isso é corrigido rápidamente
+	*se o lagarto estiver em outro lugar.*/
 	temp_ar_sector = ClimaTempo::get_instance()->get_temp_ar();
 	temp_solo_sector = ClimaTempo::get_instance()->get_temp_solo();
 	temp_toca_sector = ClimaTempo::get_instance()->get_temp_toca();
 	umidade_relativa_sector = ClimaTempo::get_instance()->get_umi_rel();
 
-	//mudana de hora, tem que atualizar as variveis tambm
-	Simdunas::get_evt_handler()->add_hook(TimeControl::get_instance()->EV_pass_hour, event_player_hour_move, this);
 	//recebe evento de mudana de setor do lagarto
 	Simdunas::get_evt_handler()->add_hook(PlayerControl::get_instance()->EV_player_move, event_player_hour_move, this);
+	//mudanca de hora, tem que atualizar as variveis tambem
+	Simdunas::get_evt_handler()->add_hook(TimeControl::get_instance()->EV_pass_hour, event_player_hour_move, this);
 }
 
 void MicroClima::event_player_hour_move(const Event*, void *data){
 
+	/*Sempre iguala as variáveis dessa classe com as variáveis ambientais da classe climaTempo.
+	* Elas só serão alteradas se o lagarto estiver em uma sombra ou em uma toca.*/
 	MicroClima::get_instance()->temp_ar_sector = ClimaTempo::get_instance()->get_temp_ar();
 	MicroClima::get_instance()->temp_solo_sector = ClimaTempo::get_instance()->get_temp_solo();
 	MicroClima::get_instance()->temp_toca_sector = ClimaTempo::get_instance()->get_temp_toca();
@@ -51,24 +57,25 @@ void MicroClima::event_player_hour_move(const Event*, void *data){
 			cout << "\n Atualizando Microclima" << endl;
 		}
 
+		/*Fatores calculados para uma redução razoável e válida dos valores das variáveis ambientais, em relação
+		*aos valores calculados na classe climaTempo.*/
 		float r = RED_SOMBRA; 
 		double a = r/(cos(5*PI/14)-1);
 		double b = 1-r-a;
 
+		//calculando temperatura do ar do microclima. Até 15h, curva crescente
 		if(TimeControl::get_instance()->get_hora() < 15){
-			MicroClima::get_instance()->temp_ar_sector = MicroClima::get_instance()->temp_ar_sector*(1-r*sin(((TimeControl::get_instance()->get_hora())-6)*PI/16));//(1-((TimeControl::get_instance()->get_hora()-6)*0.028));			
+			MicroClima::get_instance()->temp_ar_sector = MicroClima::get_instance()->temp_ar_sector*(1-r*sin(((TimeControl::get_instance()->get_hora())-6)*PI/16));			
 		}else{
 			MicroClima::get_instance()->temp_ar_sector = MicroClima::get_instance()->temp_ar_sector*(b+a*cos(((TimeControl::get_instance()->get_hora())-15)*PI/14));
-			/*if((TimeControl::get_instance()->get_hora() > 10) && TimeControl::get_instance()->get_hora() < 15 ){
-				MicroClima::get_instance()->temp_ar_sector = MicroClima::get_instance()->temp_ar_sector*0.85;
-			}else{
-				MicroClima::get_instance()->temp_ar_sector = MicroClima::get_instance()->temp_ar_sector*(0.85 + ((TimeControl::get_instance()->get_hora() - 14)*0.3));
-			}*/
 		}
 
+		//Calcula temperatura do solo do microclima
 		MicroClima::get_instance()->temp_solo_sector = (ALFA_SOLO*(MicroClima::get_instance()->temp_ar_sector)*(MicroClima::get_instance()->temp_ar_sector)) + BETA_SOLO;
+		//Calcula temperatura das tocas do microclima
 		MicroClima::get_instance()->temp_toca_sector = (ALFA_TOCA*(MicroClima::get_instance()->temp_ar_sector)) + BETA_TOCA;
 
+		//Na sombra, a temperatura do solo não será maior que 40º
 		if(MicroClima::get_instance()->temp_solo_sector>40)
 			MicroClima::get_instance()->temp_solo_sector=40;
 
@@ -116,11 +123,13 @@ double MicroClima::get_umidade_relativa_sector(){
 	return umidade_relativa_sector;
 }
 
+//Descarregamento da memória, para permitir reinicialização
 void MicroClima::unload_microClima(){
 	MicroClima::instanceFlag = false;
 	MicroClima::single = NULL;
 }
 
+//Implementação do singleton
 PT(MicroClima) MicroClima::get_instance(){
 	if(!instanceFlag){
 		if(DEBUG){
