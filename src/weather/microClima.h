9dd668aec456a214d903b70bb6f5af764222cd28
Emerson Oliveira <emerson.sdoliveira@gmail.com>
2010-03-31 00:18:29 +0000

    Documentada

--- src/weather/microClima.h
+++ src/weather/microClima.h
@@ -10,6 +10,11 @@ class Player;
 class ClimaTempo;
 class PlayerControl;
 
+/*Clase responsável por calcular o valor das variáveis ambientais (temperaturas e umidade relativa do ar)
+*na microregião onde o personagem se encontra. Para isso ela 'recebe' os eventos de tanto de hora, quando
+*atualiza os valores base, gerados pela classe climaTempo, quanto de movimentação do personagem, pois,
+*a depender de onde ele esteja (exposto ao Sol, na sombra, em alguma toca), os valores de temperatura e
+*umidade relativa do ar mudam.*/
 class MicroClima : public TypedReferenceCount{
 
 public:
@@ -21,6 +26,7 @@ public:
 	double get_temp_toca_sector();
 	double get_umidade_relativa_sector();
 
+	//Assina evento que informa passagem de hora (virtual), ou de movimentação do lagarto.
 	static void event_player_hour_move(const Event *, void *data);
 
 	//Singleton
@@ -35,9 +41,14 @@ public:
 private:
 	void atualizar_variaveis();
 
+	//armazena temperatura do ar do setor em que o lagarto (personagem) se encontra
 	double temp_ar_sector;
+	//armazena temperatura do solo do setor em que o lagarto (personagem) se encontra
 	double temp_solo_sector;
+	/*armazena temperatura da toca (aproximadamente 20 cm abaixo da superfície) do setor 
+	*em que o lagarto (personagem) se encontra*/
 	double temp_toca_sector;
+	//armazena umidade relativa do ar do setor em que o lagarto (personagem) se encontra
 	double umidade_relativa_sector;
 
 	static bool instanceFlag;
