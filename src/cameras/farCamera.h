3aade8747746d6c16be55ba1aae089820c3b1d65
Jairo Calmon <heekinho@gmail.com>
2010-12-02 05:52:05 +0000

    Tirando quebras de linha.

--- src/cameras/farCamera.h
+++ src/cameras/farCamera.h
@@ -1,32 +1,16 @@
 #ifndef FARCAMERA_H
-
 #define FARCAMERA_H
-
-
 
 #include "cameraNode.h"
 
-
-
 /*! A camera fica parada em um determinado ponto, olhando para o player/objeto */
-
 class FarCamera : public CameraNode {
-
 public:
-
 	FarCamera(PT(Camera) camera);
 
-
-
 	virtual void update();
-
 	virtual void activate();
-
 	//virtual void deactivate();
-
 };
 
-
-
 #endif
-
