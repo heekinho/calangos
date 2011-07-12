a4e57e112f2f69f645864531d3f35ec4d0c73fc0
Jairo Calmon <heekinho@gmail.com>
2011-07-12 03:20:40 +0000

    Botão de dois estados.

---
+++ src/gui/widgets/toggleButton.cxx
@@ -0,0 +1,45 @@
+/*
+ *  toggleButton.cxx
+ *  Created on: 11/07/2011
+ *  Author: heekinho
+ */
+
+#include "toggleButton.h"
+
+#include "simdunas.h"
+#include "mouseButton.h"
+
+ToggleButton::ToggleButton(const string &name, const string &text_on, const string &text_off, bool state,
+		const NodePath &parent, float valign, PT(TextFont) font, float text_size){
+	/* Configurações */
+	_state = state;
+	_text_on = text_on;
+	_text_off = text_off;
+
+	/* Configura o botão */
+	_button = new Button(name, _state ? _text_on : _text_off, font, 1.0);
+	string mouse_event = _button->get_click_event(MouseButton::one());
+	event_handler->add_hook(mouse_event, toggle, this);
+
+	/* Configura o Nodepath do botão */
+	_np_button = parent.attach_new_node(_button);
+	_np_button.set_scale(0.06);
+	_np_button.set_z(valign);
+}
+
+ToggleButton::~ToggleButton(){
+	_button = NULL;
+	_np_button.remove_node();
+}
+
+/*! Define o estado do botão e redefine o texto */
+void ToggleButton::set_state(bool state){
+	_state = state;
+	_button->set_text(_state ? _text_on : _text_off);
+}
+
+/*! Alterna o estado do botão através de eventos */
+void ToggleButton::toggle(const Event*, void* data){
+	ToggleButton* button = (ToggleButton*) data;
+	button->set_state(!button->_state);
+}
---
+++ src/gui/widgets/toggleButton.h
@@ -0,0 +1,34 @@
+/*
+ *  toggleButton.h
+ *  Created on: 11/07/2011
+ *  Author: heekinho
+ */
+
+#ifndef TOGGLEBUTTON_H
+#define TOGGLEBUTTON_H
+
+#include "button.h"
+
+/*TODO: Herança de Button cairia bem...? */
+class ToggleButton : public ReferenceCount {
+public:
+	ToggleButton(const string &name, const string &text_on, const string &text_off, bool state,
+			const NodePath &parent, float valign, PT(TextFont) font, float text_size);
+
+	~ToggleButton();
+
+	void set_state(bool state);
+	static void toggle(const Event*, void* data);
+
+//private:
+	PT(Button) _button;
+	NodePath _np_button;
+
+	string _text_on;
+	string _text_off;
+
+	bool _state;
+};
+
+
+#endif /* TOGGLEBUTTON_H */
