e5cbca88bbeb214d9a1adb7a6cb94bdba2435c2d
Jonatas Correia <scjonatas@gmail.com>
2011-11-23 18:16:31 +0000

    continuando a implementar a classe window.

--- src/gui/widgets/window.cxx
+++ src/gui/widgets/window.cxx
@@ -6,24 +6,45 @@
  */
 
 #include "window.h"
+#include "fontPool.h"
 
-//TODO gets e sets.
-//TODO header
 //TODO efeito ao abrir
 //TODO padronizar forma de fechar
-Window::Window(NodePath parent, float width, float height, string name) {
-	frame = new PGVirtualFrame(name);
+//TODO gets e sets.
+Window::Window(NodePath parent, float width, float height, string heade) {
+	frame = new PGVirtualFrame("window");
 	frame->setup(width, height);
 	PGFrameStyle style = frame->get_frame_style(frame->get_state());
 	style.set_type(PGFrameStyle::T_flat);
 	frame->set_frame_style(frame->get_state(), style);
 
-	frame_np = NodePath(frame);
-	frame_np.reparent_to(parent);
-	//Seta a posição do frame
-	frame_np.set_pos(-0.8, 0.0, -0.8);
-	//Seta a cor do frame
-	frame_np.set_color(0.97, 0.945, 0.914);
+	np_frame = NodePath(frame);
+	np_frame.reparent_to(parent);
+	np_frame.set_pos(-0.8, 0.0, -0.8);
+	np_frame.set_transparency(TransparencyAttrib::M_alpha);
+	np_frame.set_color(0.97, 0.945, 0.914, 0.85);
+
+	header_frame = new PGVirtualFrame("window_header");
+	header_frame->setup(width, 0.1);
+	style.set_type(PGFrameStyle::T_flat);
+	header_frame->set_frame_style(header_frame->get_state(), style);
+
+	np_header_frame = NodePath(header_frame);
+	np_header_frame.reparent_to(parent);
+	np_header_frame.set_pos(-0.8, 0.0, 0.2);
+	np_header_frame.set_color(1, 1, 1);
+
+	header = new TextNode("header");
+	header->set_text("Realizações");
+	header->set_font(FontPool::load_font("models/gui/fonts/suplexcomic-large"));
+	np_header = np_header_frame.attach_new_node(header);
+	np_header.set_scale(0.07);
+	np_header.set_pos((width / 2.0) - (header->get_width() * np_header.get_sx() / 2.0), 0, (header->get_height() * np_header.get_sz() / 2.0) - 0.015);
+	np_header.set_color(0, 0, 0, 1);
+
+	header_frame->setup(1, 0.1);
 }
 
 Window::~Window() {}
+
+
--- src/gui/widgets/window.h
+++ src/gui/widgets/window.h
@@ -16,9 +16,22 @@ public:
 	Window(NodePath parent, float width = 2, float height = 1, string name = "frame");
 	virtual ~Window();
 
+	void set_header(string text);
+	string get_header();
+	void set_width(float width);
+	float get_width();
+	void set_pos_x(float x);
+	float get_pos_x();
+	void set_pos_y(float y);
+	float get_pos_y();
+
 private:
 	PT(PGVirtualFrame) frame;
-	NodePath frame_np;
+	NodePath np_frame;
+	PT(PGVirtualFrame) header_frame;
+	NodePath np_header_frame;
+	PT(TextNode) header;
+	NodePath np_header;
 };
 
 #endif /* WINDOW_H */
