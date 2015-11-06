4aea173f2397406ad33d00a76ad9595280df8192
Crystal Campos <campos_537@hotmail.com>
2015-11-06 18:25:47 +0000

    Foi adicionado a escolha da imagem de acordo com a linguagem escolha

--- src/gui/screen/inGame/window/keyInstructionsWindow.cxx
+++ src/gui/screen/inGame/window/keyInstructionsWindow.cxx
@@ -6,9 +6,12 @@
  */
 
 #include "keyInstructionsWindow.h"
+#include "imageRepository.h"
 #include "fontPool.h"
 #include "mouseButton.h"
 #include "simdunas.h"
+#include "inGameScreenManager.h"
+#include "pauseScreen.h"
 
 KeyInstructionsWindow::KeyInstructionsWindow(NodePath parent, float width, float height, string title, float pos_x, float pos_y) : Window(parent, width, height, title, pos_x, pos_y) {
 
@@ -17,9 +20,14 @@ KeyInstructionsWindow::KeyInstructionsWindow(NodePath parent, float width, float
 
 	// Substituir a herança de Window para Screen. Inserir um botão "Fechar" para esconder a imagem, ou seja, chamar o método hide()
 
-	img_teclas = window->load_model(parent, "models/gui/teclas.png");
-	img_teclas.set_scale(0.075, 0.0004, 0.06);
-	img_teclas.set_pos(0.0, 0.0, 0.0);
+	//img_teclas = window->load_model(parent, "models/gui/teclas.png");
+	//img_teclas.set_scale(0.075, 0.0004, 0.06);
+	//img_teclas.set_pos(0.0, 0.0, 0.0);
+
+	img_teclas = ImageRepository::get_instance()->get_image("teclas");
+	img_teclas.reparent_to(np_frame);
+	img_teclas.set_scale(0.10);
+	img_teclas.set_pos(1.00, 0.0, 0.75);
 //	lb_teclas = new Text("lb_teclas", "Teclas", np_frame, TextNode::get_default_font(), 0.05);
 //
 //	np_lb_teclas = get_root().attach_new_node(lb_teclas);
@@ -33,9 +41,9 @@ KeyInstructionsWindow::~KeyInstructionsWindow() {
 }
 
 void KeyInstructionsWindow::load(NodePath parent) {
-	img_teclas = window->load_model(parent, "models/gui/teclas.png");
-	img_teclas.set_scale(0.075, 0.0004, 0.06);
-	img_teclas.set_pos(0.0, 0.0, 0.0);
+	//img_teclas = window->load_model(parent, "models/gui/teclas.png");
+	//img_teclas.set_scale(0.075, 0.0004, 0.06);
+	//img_teclas.set_pos(0.0, 0.0, 0.0);
 }
 
 void KeyInstructionsWindow::hide() {
